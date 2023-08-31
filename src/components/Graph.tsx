edda34aabdb68a417b1d5bc8a69fb3fd98e8e47a
huanxiqiao <huanxiqiao@qq.com>
2023-08-31 20:26:59 +0800

    0831

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,13 +1,14 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
-import "./Graph.css";
+import "./styles/Graph.css";
 
 interface HierarchyNode {
-  name: string;
-  version: number;
-  children: HierarchyNode[];
-  hasCircularDependency?: boolean;
-  hasDuplicateDependency?: boolean;
+  name: string; // 节点名称
+  version: string; // 版本号
+  children: HierarchyNode[]; // 子节点
+  hasCircularDependency?: boolean; // 标记循环依赖
+  hasDuplicateDependency?: boolean; // 标记重复依赖
+  totalDependencies: number; // 依赖总数
 }
 
 function convertHierarchy(data: any, ancestors: string[] = []): HierarchyNode {
@@ -15,6 +16,7 @@ function convertHierarchy(data: any, ancestors: string[] = []): HierarchyNode {
     name: data.name,
     version: data.version,
     children: [],
+    totalDependencies: 0,
   };
 
   let hasCircularDependency = false;
@@ -39,6 +41,12 @@ function convertHierarchy(data: any, ancestors: string[] = []): HierarchyNode {
       const childNode = convertHierarchy(dep, ancestors);
       root.children.push(childNode);
 
+      if (childNode.children.length === 0) {
+        root.totalDependencies += 1;
+      } else {
+        root.totalDependencies += childNode.totalDependencies;
+      }
+
       ancestors.pop();
     });
 
@@ -51,87 +59,200 @@ function convertHierarchy(data: any, ancestors: string[] = []): HierarchyNode {
 
 function mergeHierarchyData(dataObjects): HierarchyNode {
   const mergedRoot: HierarchyNode = {
-    name: "mergedRoot",
-    version: 0,
+    name: "Project Name: ##########",
+    version: "=====",
     children: [],
+    totalDependencies: 0,
   };
+
   dataObjects.forEach((data) => {
     const hierarchyData = convertHierarchy(data);
     mergedRoot.children.push(hierarchyData);
+    mergedRoot.totalDependencies += hierarchyData.totalDependencies;
   });
   return mergedRoot;
 }
 
-function createSvg() {
+function drawNodelinks(divElement, data: HierarchyNode) {
+  const localData = data;
+  let localIndex = 0;
+  let margin = { top: 20, left: 20, right: 20 },
+    i = 0,
+    nodeSize = 20,
+    duration = 500,
+    format = d3.format(",");
+
+  let nodeEnterTransition = d3.transition().duration(750).ease(d3.easeLinear);
+
+  const root = d3.hierarchy(data).eachBefore((d) => (d.index = i++));
+  const nodeslength = root.descendants();
+
+  let columns = [
+    {
+      label: "NPM Package Analyzer",
+      value: null,
+      format,
+      x: 140,
+      render: (text) => null,
+    },
+    {
+      label: "Version",
+      value: (d) => d.version,
+      format,
+      x: window.innerWidth - 400,
+      render: (text) => text.data.version,
+    },
+    {
+      label: "Dependencies",
+      value: (d) => d.hasCircularDependency,
+      format: (value, d) => (d.children ? format(value) : "-"),
+      x: window.innerWidth - 300,
+      render: (text) => text.data.totalDependencies,
+    },
+  ];
+
+  d3.selectAll("svg").remove();
   const svg = d3
-    .select("#graph-svg")
-    .attr("width", "100%")
-    .attr("height", "100%");
-  const width = svg.node().getBoundingClientRect().width;
-  const height = svg.node().getBoundingClientRect().height;
-  return { svg, width, height };
-}
+    .select(divElement)
+    .append("svg")
+    .attr("viewBox", [
+      -nodeSize / 2,
+      -(nodeSize * 3) / 2,
+      window.innerWidth - 200,
+      (nodeslength.length + 1) * nodeSize,
+    ])
+    .attr("font-family", "sans-serif")
+    .attr("font-size", 10)
+    .style("overflow", "visible");
 
-function drawNodelinks(svg, width, height, hierarchyData) {
-  const nodelinkG = svg
-    .append("g")
-    .attr("class", "nodelinkG")
-    .attr("transform", `translate(30,10)`);
-  const treeLayout = d3.tree().size([height - 50, width / 1.2]);
-  const root = d3.hierarchy(hierarchyData);
-  const links = treeLayout(root).links();
-  const nodes = treeLayout(root).descendants();
+  function update() {
+    const nodes = root.descendants();
 
-  const link = nodelinkG
-    .selectAll("path")
-    .data(links)
-    .join("path")
-    .attr(
-      "d",
-      d3
-        .linkHorizontal()
-        .x((d) => d.y)
-        .y((d) => d.x)
-    )
-    .attr("fill", "none")
-    .attr("stroke", "grey")
-    .attr("stroke-width", 0.5);
+    const node = svg
+      .selectAll(".node")
+      .data(nodes, (d) => d.id || (d.id = ++i));
 
-  const nodeWidth = d3.max(nodes, (d) => d.data.name.length * 8);
+    const link = svg
+      .append("g")
+      .attr("fill", "none")
+      .attr("stroke", "#aac4ff")
+      .selectAll("path")
+      .data(root.links())
+      .join("path")
+      .attr(
+        "d",
+        (d) => `
+        M${d.source.depth * nodeSize},${d.source.index * nodeSize}
+        V${d.target.index * nodeSize}
+        h${nodeSize}
+      `
+      )
+      .attr("class", (d) => `link${d.source.index}`);
 
-  const node = nodelinkG
-    .selectAll("rect")
-    .data(nodes)
-    .join("circle")
-    .attr("cx", (d) => d.y)
-    .attr("cy", (d) => d.x)
-    .attr("r", 3)
-    .attr("fill", (d) => (d.data.hasCircularDependency ? "red" : "#ccc"))
-    .attr("stroke", "black")
-    .attr("stroke-width", 1)
-    .attr("stroke-opacity", 0.5);
+    const nodeEnter = node.enter().append("g").attr("class", "node");
 
-  const label = nodelinkG
-    .selectAll("text")
-    .data(nodes)
-    .join("text")
-    .attr("dy", "0.31em")
-    .attr("x", (d) => d.y + 4)
-    .attr("y", (d) => d.x)
-    .text((d) => d.data.name)
-    .attr("font-size", 8)
-    .attr("fill", "black")
-    .attr("text-anchor", "start");
+    nodeEnter
+      .attr("transform", (d) => `translate(0,${d.index * nodeSize})`)
+      .on("click", (event) => click(event.target.__data__));
+
+    nodeEnter
+      .append("circle")
+      .attr("cx", (d) => d.depth * nodeSize)
+      .attr("r", 3)
+      .attr("fill", (d) => (d.children ? "#2863ed" : "#d2daff"))
+      .attr("stroke", "black")
+      .attr("stroke-width", 1)
+      .attr("stroke-opacity", 0.5);
+
+    nodeEnter
+      .append("rect")
+      .attr("x", (d) => d.depth * nodeSize + 6)
+      .attr("y", -nodeSize / 2 + 3)
+      .attr("width", (d) => d.data.name.length * 7.5)
+      .attr("height", 15)
+      .attr("fill", "#d2daff")
+      .attr("stroke", "black")
+      .attr("rx", 3)
+      .attr("ry", 3)
+      .attr("opacity", (d) => (d.depth == 0 ? 0 : 0.3));
+
+    nodeEnter
+      .append("text")
+      .attr("dy", "0.32em")
+      .attr("x", (d) => d.depth * nodeSize + 8)
+      .text((d) => d.data.name)
+      .attr("font-size", (d) => (d.depth == 0 ? "13px" : "12px"));
+
+    nodeEnter.append("title").text((d) =>
+      d
+        .ancestors()
+        .reverse()
+        .map((d) => d.data.name)
+        .join("/")
+    );
+
+    for (const { label, value, format, x, render } of columns) {
+      svg
+        .append("text")
+        .attr("dy", "0.32em")
+        .attr("y", -nodeSize)
+        .attr("x", x)
+        .attr("text-anchor", "end")
+        .attr("font-weight", "bold")
+        .attr("font-size", "13px")
+        .attr("fill", "#2863ed")
+        .text(label);
+
+      nodeEnter
+        .append("text")
+        .attr("dy", "0.32em")
+        .attr("x", x)
+        .attr("text-anchor", "end")
+        .text((d) => render(d))
+        .attr("font-size", "12px");
+    }
+
+    node.exit().transition().duration(duration).style("opacity", 0).remove();
+
+    link.exit().transition().duration(duration).style("opacity", 0).remove();
+  }
+
+  function click(d) {
+    const allPaths: number[] = [];
+    allPaths.push(d.index);
+    if (d.parent) {
+      if (d.children) {
+        d._children = d.children;
+        d.children = null;
+        const allChildren = d.descendants();
+        allChildren[0]["_children"].forEach(function (c) {
+          allPaths.push(c.index);
+          c.descendants().forEach((gc) => {
+            allPaths.push(gc.index);
+          });
+        });
+      } else {
+        d.children = d._children;
+        d._children = null;
+      }
+      allPaths.map((p) => d3.selectAll(`path.link${p}`).remove());
+      d3.select(this).remove();
+
+      update();
+    }
+  }
+
+  update();
+  return svg.node();
 }
 
 export default function Graph() {
-  const svgRef = useRef(null);
+  const [jsonData, setJsonData] = useState([]);
 
   useEffect(() => {
-    const { svg, width, height } = createSvg();
-
     const fetchData = async () => {
       try {
+        // const response = await fetch('http://localhost:3030')
         const response = await fetch("./result.json");
         const data = await response.json();
 
@@ -143,10 +264,10 @@ export default function Graph() {
           data[4],
         ]);
 
-        console.log(mergedHierarchyData);
-        drawNodelinks(svg, width, height, mergedHierarchyData);
+        const divElement = document.getElementById("graph-pane");
 
-        return data;
+        drawNodelinks(divElement, mergedHierarchyData);
+        setJsonData(data);
       } catch (error) {
         console.error("Error fetching data:", error);
         return [];
@@ -155,9 +276,5 @@ export default function Graph() {
     fetchData();
   }, []);
 
-  return (
-    <div id="graph-pane">
-      <svg ref={svgRef} id="graph-svg"></svg>
-    </div>
-  );
+  return <div id="graph-pane"></div>;
 }
